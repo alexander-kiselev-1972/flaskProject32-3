178f9d8022d4e11530ef76a38c87be8639ed8617
rotor1972 <aptvnuk@gmail.com>
2021-04-28 13:46:31 +0200

    админка без аутентификации

--- app/__init__.py
+++ app/__init__.py
@@ -30,7 +30,15 @@ def create_app(config_name):
         moment.init_app(app)
         db.init_app(app)
         login_manager.init_app(app)
-        #admin.init_app(app)
+        admin.init_app(app)
+
+        from .auth import auth as auth_blueprint
+        app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+        # from .admin import admin as admin_bp
+        # app.register_blueprint(admin_bp)
+
+
 
         from .main import main as main_blueprint
         app.register_blueprint(main_blueprint)
